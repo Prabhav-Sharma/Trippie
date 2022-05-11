1ed153a81774cd62079df3e90b823ffd2ce3ab65
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-11 10:45:58 +0530

    Feat: Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      robotoFlex: "Roboto Flex, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
