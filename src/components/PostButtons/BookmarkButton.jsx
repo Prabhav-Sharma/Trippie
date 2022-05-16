28c4340c099dcbd89d1f9e7839a6bf21cc14533d
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-16 17:45:31 +0530

    Feat: Add Bookmark button

---
+++ src/components/PostButtons/BookmarkButton.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addPostToBookmarks,
+  fetchBookmarks,
+  removePostFromBookmarks,
+} from "../../services";
+import { BsBookmark, BsFillBookmarkCheckFill } from "../../Utils/icons";
+
+function BookmarkButton({ postId }) {
+  const token = useSelector((state) => state.auth.token);
+  const bookmarks = useSelector((state) => state.user.bookmarks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchBookmarks(token, dispatch);
+  }, []);
+
+  return bookmarks.some((post) => post._id === postId) ? (
+    <BsFillBookmarkCheckFill
+      className="text-xl text-sky-500 hover:md:scale-110 cursor-pointer"
+      onClick={() => removePostFromBookmarks(postId, token, dispatch)}
+    />
+  ) : (
+    <BsBookmark
+      className="text-xl hover:text-sky-500 hover:md:scale-110 cursor-pointer"
+      onClick={() => addPostToBookmarks(postId, token, dispatch)}
+    />
+  );
+}
+
+export default BookmarkButton;
