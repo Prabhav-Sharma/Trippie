301a5b0cef89485bc01fa5518c4e3070b4db16b8
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-18 15:45:42 +0530

    Feat: Increased Reusability of Components

--- src/components/PostButtons/LikeButton.jsx
+++ src/components/PostButtons/LikeButton.jsx
@@ -2,21 +2,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { AiOutlineHeart, AiFillHeart } from "../../Utils/icons";
 import { likePost, dislikePost } from "../../services";
 
-function LikeButton({ postId, likedBy }) {
+function LikeButton({ postId, likedBy, location }) {
   const userId = useSelector((state) => state.user._id);
   const token = useSelector((state) => state.auth.token);
 
   const dispatch = useDispatch();
-
   return likedBy.some((user) => user._id === userId) ? (
     <AiFillHeart
       className="text-xl text-sky-500 hover:md:scale-110 cursor-pointer"
-      onClick={() => dislikePost(postId, token, dispatch)}
+      onClick={() => dislikePost(postId, token, dispatch, location === "POST")}
     />
   ) : (
     <AiOutlineHeart
       className="text-xl hover:text-sky-500 hover:md:scale-110 cursor-pointer"
-      onClick={() => likePost(postId, token, dispatch)}
+      onClick={() => likePost(postId, token, dispatch, location === "POST")}
     />
   );
 }
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,10 +1,28 @@
 import dayjs from "dayjs";
-import { BiComment, FiShare } from "../Utils/icons";
-import { LikeButton } from ".";
-import BookmarkButton from "./PostButtons/BookmarkButton";
+import {
+  BiComment,
+  FiShare,
+  BsThreeDotsVertical,
+  FaEdit,
+  FaTrash,
+} from "../Utils/icons";
+import { LikeButton, BookmarkButton } from ".";
 import { likeFormatter } from "../Utils/helpers";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useToggle } from "../hooks";
+import { deletePost } from "../services";
 
-function PostCard({ post }) {
+function PostCard({ post, location = "HOME" }) {
+  const authUsername = useSelector((state) => state.user.username);
+  const token = useSelector((state) => state.auth.token);
+  const navigate = useNavigate();
+  const browserLocation = useLocation();
+  const dispatch = useDispatch();
+
+  const { toggle: optionsToggle, setToggle: setOptionsToggle } =
+    useToggle(false);
+
   const {
     username,
     createdAt,
@@ -15,7 +33,10 @@ function PostCard({ post }) {
   } = post;
 
   return (
-    <div className="flex flex-row bg-transparent border border-slate-500 gap-3 p-3 text-white">
+    <div
+      className="flex flex-row relative bg-transparent border-y border-slate-500 gap-3 p-3 text-white"
+      onClick={() => navigate(`/post/${_id}`)}
+    >
       <div className="profile-icon">
         <img className="rounded-full " src={profileImg} alt={username} />
       </div>
@@ -29,16 +50,62 @@ function PostCard({ post }) {
         <p className="text-xs sm:text-sm md:text-base whitespace-pre-wrap">
           {content}
         </p>
-        <div className="w-full  flex flex-row justify-between px-2 self-center ">
+        <div
+          className="w-full  flex flex-row justify-between px-2 self-center "
+          onClick={(e) => e.stopPropagation()}
+        >
           <span className="flex items-center gap-1">
-            <LikeButton postId={_id} likedBy={likedBy} />
+            <LikeButton postId={_id} likedBy={likedBy} location={location} />
             {likeFormatter(likeCount)}
           </span>
-          <BiComment className="text-xl hover:text-sky-500 hover:md:scale-110 cursor-pointer " />
+          <BiComment
+            className="text-xl hover:text-sky-500 hover:md:scale-110 cursor-pointer "
+            onClick={() => navigate(`/post/${_id}`, { state: "COMMENT" })}
+          />
           <FiShare className="text-xl hover:text-sky-500 hover:md:scale-110 cursor-pointer" />
           <BookmarkButton postId={_id} />
         </div>
       </div>
+      {authUsername === username && (
+        <span
+          className="absolute top-4 right-4"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <BsThreeDotsVertical
+            className={`${
+              optionsToggle && "bg-slate-700"
+            } text-3xl hover:cursor-pointer p-1.5 rounded-md`}
+            onClick={() => setOptionsToggle((value) => !value)}
+          />
+          <ul
+            className={` ${
+              optionsToggle ? "absolute" : "hidden"
+            } p-1 h-max w-20 max-h-32 flex flex-col toggle-menu gap-2 items-start bg-gray-800 border border-solid rounded-md border-gray-500 -top-0 -left-21 ml`}
+          >
+            <li
+              className="hover:cursor-pointer  w-full rounded-md p-1 flex items-center gap-1 hover:bg-slate-700 duration-100 ease-linear"
+              onClick={() =>
+                navigate(`/edit/${_id}`, {
+                  state: { from: browserLocation },
+                })
+              }
+            >
+              <FaEdit />
+              Edit
+            </li>
+            <li
+              className="hover:cursor-pointer w-full text-red-500 flex rounded-md p-1 items-center gap-1 hover:bg-slate-700 duration-100 ease-linear"
+              onClick={() => {
+                deletePost(_id, token, dispatch);
+                location === "POST" && navigate("/home");
+              }}
+            >
+              <FaTrash />
+              Delete
+            </li>
+          </ul>
+        </span>
+      )}
     </div>
   );
 }
