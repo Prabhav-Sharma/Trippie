7084bc006f7803c279ae7257c54f5f5b11d4d712
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-18 23:39:39 +0530

    Feat: Add Active/inactive styles for NavLink

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -2,33 +2,61 @@ import { NavLink } from "react-router-dom";
 import {
   GiShipWheel,
   GiPlagueDoctorProfile,
-  BsBookmark,
-  AiOutlineHome,
-  BsBell,
+  BsFillBookmarkCheckFill,
+  AiFillHome,
+  BsBellFill,
 } from "../Utils/icons";
 
 function SideNav() {
   const tabStyles =
-    "flex flex-row gap-2 text-white text-xl items-center hover:text-blue-400";
+    "flex flex-row gap-2 text-xl items-center ease-linear, duration-200";
+
+  const activeStyles = "text-blue-400";
+  const inactiveStyles = "text-white hover:text-blue-400";
   return (
     <aside className="fixed bottom-0 border-t-2 p-4 w-full z-10 bg-gray-800 border-blue-500 sm:border-slate-400 sm:border-l-0 sm:border sm:gap-10 flex flex-row font-robotoFlex sm:rounded-md lg:mt-2 justify-around sm:flex-col sm:static sm:bottom-none sm:w-max sm:justify-start lg:items-start lg:w-64 lg:h-max lg:ml-1 lg:border-none ">
-      <NavLink to="/home" className={`${tabStyles}`}>
-        <AiOutlineHome />
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          `  ${isActive ? activeStyles : inactiveStyles} ${tabStyles}`
+        }
+      >
+        <AiFillHome />
         <span className="hidden text-base lg:inline">Home</span>
       </NavLink>
-      <NavLink to="/login" className={`${tabStyles}`}>
-        <BsBookmark />
+      <NavLink
+        to="/bookmarks"
+        className={({ isActive }) =>
+          `  ${isActive ? activeStyles : inactiveStyles} ${tabStyles}`
+        }
+      >
+        <BsFillBookmarkCheckFill className="text-lg" />
         <span className="hidden text-base lg:inline"> Bookmarks</span>
       </NavLink>
-      <NavLink to="/home" className={`${tabStyles}`}>
+      <NavLink
+        to="/explore"
+        className={({ isActive }) =>
+          `  ${isActive ? activeStyles : inactiveStyles} ${tabStyles}`
+        }
+      >
         <GiShipWheel />
         <span className="hidden text-base lg:inline">Explore</span>
       </NavLink>
-      <NavLink to="/home" className={`${tabStyles}`}>
-        <BsBell />
+      <NavLink
+        to="/notifications"
+        className={({ isActive }) =>
+          `  ${isActive ? activeStyles : inactiveStyles} ${tabStyles}`
+        }
+      >
+        <BsBellFill />
         <span className="hidden text-base lg:inline"> Notifications</span>
       </NavLink>
-      <NavLink to="/home" className={`${tabStyles}`}>
+      <NavLink
+        to="/profile"
+        className={({ isActive }) =>
+          `  ${isActive ? activeStyles : inactiveStyles} ${tabStyles}`
+        }
+      >
         <GiPlagueDoctorProfile />
         <span className="hidden text-base lg:inline">Profile</span>
       </NavLink>
