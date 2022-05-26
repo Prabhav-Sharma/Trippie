0c68f1e9a3b00fdc800b6bc673374f759fc5dc35
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-26 22:07:59 +0530

    Feat: Add Overflow property to section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,19 @@
-import { useEffect } from "react";
 import { useDocumentTitle } from "../hooks";
 import { useDispatch, useSelector } from "react-redux";
 import { Feed, NewContent } from "../components";
-import { fetchPosts, addPost } from "../services";
+import { addPost } from "../services";
 
 function Home() {
-  useDocumentTitle("Home");
   const posts = useSelector((state) => state.appData.posts);
   const token = useSelector((state) => state.auth.token);
   const following = useSelector((state) => state.user.following);
   const authUserId = useSelector((state) => state.user._id);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    fetchPosts(dispatch);
-  }, []);
+  useDocumentTitle("Home");
 
-  const addPostHandler = async (text) => {
-    return await addPost({ content: text.trim() }, token, dispatch);
+  const addPostHandler = async (content) => {
+    return await addPost(content, token, dispatch);
   };
 
   return (
